59d1dcb348fcbb24cfe0f0fe90e4d1e0cf190e41
Julian Thomassie <julianthomassie@gmail.com>
2021-07-29 21:10:32 -0700

    WIP: recursive build pattern

--- src/yaml.rs
+++ src/yaml.rs
@@ -4,44 +4,7 @@ use serde::Deserialize;
 use std::fs::File;
 use std::io::BufReader;
 use std::path::PathBuf;
-use whoami::Platform;
-
-#[derive(Debug)]
-pub struct Locale {
-    user: String,
-    os: Platform,
-}
-
-impl Locale {
-    pub fn auto() -> Self {
-        Locale {
-            user: whoami::username(),
-            os: whoami::platform(),
-        }
-    }
-}
-
-#[derive(Debug, PartialEq, Deserialize)]
-pub struct BuildCase {
-    user: Option<String>,
-    os: Option<String>,
-    pub link: Vec<Link>,
-}
-
-impl PartialEq<Locale> for &BuildCase {
-    fn eq(&self, other: &Locale) -> bool {
-        // TODO: implement complete locale matching
-        self.user == None && self.os == None
-    }
-}
-
-#[derive(Debug, PartialEq, Deserialize)]
-#[serde(rename_all(deserialize = "snake_case"))]
-pub enum BuildScope {
-    All(BuildCase),
-    Case(BuildCase),
-    Default(BuildCase),
-}
+use whoami;
 
 #[derive(Debug, PartialEq, Deserialize)]
 pub struct Repo {
@@ -50,32 +13,102 @@ pub struct Repo {
 }
 
 #[derive(Debug, PartialEq, Deserialize)]
+pub struct Locale {
+    user: Option<String>,
+    platform: Option<String>,
+    distro: Option<String>,
+}
+
+impl Locale {
+    pub fn auto() -> Self {
+        Self {
+            user: Some(whoami::username()),
+            platform: Some(format!("{:?}", whoami::platform())),
+            distro: whoami::distro().split(" ").next().map(String::from),
+        }
+    }
+
+    pub fn is_subset(&self, other: &Self) -> bool {
+        (self.user.is_none() || self.user == other.user)
+            && (self.platform.is_none() || self.platform == other.platform)
+            && (self.distro.is_none() || self.distro == other.distro)
+    }
+}
+
+#[derive(Debug, PartialEq, Deserialize)]
+#[serde(rename_all(deserialize = "snake_case"))]
+pub enum Case {
+    Local { spec: Locale, build: Vec<BuildUnit> },
+    Default { build: Vec<BuildUnit> },
+}
+
+#[derive(Debug, PartialEq, Deserialize)]
+#[serde(rename_all(deserialize = "snake_case"))]
+pub enum BuildUnit {
+    Case(Vec<Case>),
+    Link(Vec<Link>),
+}
+
+impl BuildUnit {
+    // Recursively remove all invalid build cases
+    // Returns True when the build ends up empty
+    pub fn prune(&mut self) -> bool {
+        match self {
+            Self::Case(cv) => {
+                // TODO: create static locale
+                let locale = Locale::auto();
+                let mut default = true;
+                cv.drain_filter(|scope| match scope {
+                    Case::Local { spec, build } if spec.is_subset(&locale) => {
+                        default = false;
+                        build.drain_filter(|u| u.prune());
+                        build.is_empty()
+                    }
+                    Case::Default { build } if default => {
+                        build.drain_filter(|u| u.prune());
+                        build.is_empty()
+                    }
+                    _ => true,
+                });
+                cv.is_empty()
+            }
+            Self::Link(_) => false,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Deserialize)]
 pub struct Build {
     repo: Repo,
-    build: Vec<BuildScope>,
+    build: Vec<BuildUnit>,
 }
 
 impl Build {
-    pub fn apply<T>(&self, f: fn(&BuildCase) -> T) -> Vec<T> {
-        let locale = Locale::auto();
-        let mut default = true;
-        self.build
-            .iter()
-            .filter_map(|scope| match scope {
-                BuildScope::All(c) => Some(f(c)),
-                BuildScope::Case(c) if c == locale => {
-                    default = false;
-                    Some(f(c))
-                }
-                BuildScope::Default(c) if default => Some(f(c)),
-                BuildScope::Default(_) => {
-                    default = true;
-                    None
-                }
-                _ => None,
-            })
-            .collect()
+    pub fn prune(&mut self) -> bool {
+        self.build.drain_filter(|u| u.prune());
+        self.build.is_empty()
     }
+
+    // pub fn apply<T>(&self, f: fn(&BuildCase) -> T) -> Vec<T> {
+    //     let locale = BuildCase::auto();
+    //     let mut default = true;
+    //     self.build
+    //         .iter()
+    //         .filter_map(|scope| match scope {
+    //             BuildScope::All(c) => Some(f(c)),
+    //             BuildScope::Case(c) if c == locale => {
+    //                 default = false;
+    //                 Some(f(c))
+    //             }
+    //             BuildScope::Default(c) if default => Some(f(c)),
+    //             BuildScope::Default(_) => {
+    //                 default = true;
+    //                 None
+    //             }
+    //             _ => None,
+    //         })
+    //         .collect()
+    // }
 }
 
 pub fn parse(path: PathBuf) -> DotsResult<Build> {
