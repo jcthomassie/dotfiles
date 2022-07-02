176b9fad734cd125de8c5c8dc002c91f79c06cfb
Julian Thomassie <julianthomassie@gmail.com>
2022-07-02 14:55:33 -0700

    Trim tree-sitter config

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -12,17 +12,9 @@ require('onedark').load()
 require("nvim-tree").setup()
 require('nvim-treesitter.configs').setup {
     ensure_installed = { "bash", "python", "rust", "yaml", "json", "lua" },
-
-    -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
-
     highlight = {
-      enable = true,
-
-      -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-      -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-      -- Using this option may slow down your editor, and you may see some duplicate highlights.
-      -- Instead of true it can also be a list of languages
-      additional_vim_regex_highlighting = false,
+        enable = true,
+        additional_vim_regex_highlighting = false,
     },
-  }
+}
