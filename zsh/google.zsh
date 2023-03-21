d8f7cbd0292c1094520360f98975468f9ea5635c
Julian Thomassie <julianth@google.com>
2023-03-21 15:55:10 -0700

    Add setup-android-branch function

--- zsh/google.zsh
+++ zsh/google.zsh
@@ -2,10 +2,24 @@ export CHROME_REMOTE_DESKTOP_RES_LAPTOP="3840x2400" # Lenovo ThinkPad X1 Carbon
 export CHROME_REMOTE_DESKTOP_RES_DESK="3840x2160" # Lenovo Monitor
 export CHROME_REMOTE_DESKTOP_RES_HOME="2560x1440" # LG Monitor
 export CHROME_REMOTE_DESKTOP_DEFAULT_DESKTOP_SIZES="$CHROME_REMOTE_DESKTOP_RES_LAPTOP,$CHROME_REMOTE_DESKTOP_RES_DESK,$CHROME_REMOTE_DESKTOP_RES_HOME"
+export ANDROID_ROOT_DIR="$HOME/android"
 
 function set-crd-res {
     xrandr -s "${1:?Missing argument RESOLUTION}"
     sudo /etc/init.d/chrome-remote-desktop restart
+}
+
+function setup-android-branch {
+    local branch="${1:?Missing argument BRANCH}"
+    local branch_dir="$ANDROID_ROOT_DIR/$branch"
+    if [[ -d $branch_dir ]]; then
+        echo "$branch_dir already exists"
+    else
+        mkdir $branch_dir
+        cd $branch_dir
+        repo init -c -u sso://googleplex-android/platform/manifest -b $branch --use-superproject --partial-clone --partial-clone-exclude=platform/frameworks/base --clone-filter=blob:limit=10M
+        repo sync -c -j64
+    fi
 }
 
 alias auth="gcertstatus --check_remaining=1h --quiet || gcert"
